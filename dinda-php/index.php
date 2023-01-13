81d0c58abd27f780cb89de6ae91fe6cd90abb6b0
Ryan <89153707+Elbanteny@users.noreply.github.com>
2023-01-13 17:43:45 +0700

    Update index.php

--- dinda-php/index.php
+++ dinda-php/index.php
@@ -5,9 +5,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Portofolio</title>
+    <title>My Profile</title>
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -17,7 +24,7 @@
     </header>
     <main>
         <div class="container">
-            <h3 class="text-center">Welome To My Profile</h3>
+            <h3 class="text-center">Welcome To My Profile</h3>
             <!-- penggunaan statement -->
             <?php include "template/state.php"; ?>
             <br>
@@ -29,20 +36,13 @@
                     <br>
                     <h4 class="text-center">Adinda Fadia Salsabila</h4>
                     <h4 class="text-center">2202041083</h4>
-                    <blockquote class="blockquote text-center">"Saya adalah Mahasiswa prodi INFORMATIKA KELAS A SEMESTER
-                        SATU"</blockquote>
+                    <blockquote class="blockquote text-center">"Mahasiswa Fakultas Teknologi Informas Prodi Informatika
+                        Kelas A Semester 1"</blockquote>
                 </div>
             </div>
         </div>
     </main>
 
-
-    <!-- <footer>
-        <div
-            class="col-md-12 text-center container sticky-bottom p-3 bg-secondary position-absolute bottom-0 start-50 translate-middle-x ">
-            <span class="text-white">&copy; RIZQI WILDAN HARIRI - INFORMATIKA KELAS A - TUGAS UAS</span>
-        </div>
-    </footer> -->
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
